2430fb73c07657aa33873eccb0a214953023ed2f
Eran Rundstein <eran@rundste.in>
2021-10-21 20:39:49 -0700

    initial work on libmobilecoin fog validated pubkey api

--- fog.go
+++ fog.go
@@ -8,14 +8,27 @@ import (
 	"fmt"
 	"math"
 	"net/url"
+    "unsafe"
+    "encoding/hex"
 
 	"github.com/MixinNetwork/mobilecoin-go/block"
 	"github.com/bwesterb/go-ristretto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+    "google.golang.org/protobuf/proto"
 )
 
-//go:embed credentials/8395.crt
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <errno.h>
+// #include "libmobilecoin.h"
+// #cgo CFLAGS: -I/Users/eran/Projects/mc/mobilecoin/libmobilecoin/include
+// #cgo LDFLAGS: /Users/eran/Projects/mc/mobilecoin/target/debug/libmobilecoin.a -framework Security -framework Foundation
+import "C"
+
+
+
+//go:embed credentials/lets-encrypt.crt
 var crt []byte
 
 const (
@@ -76,4 +89,113 @@ func GetFogReportResponse(address string) (*block.ReportResponse, error) {
 
 	in := &block.ReportRequest{}
 	return client.GetReports(context.Background(), in)
+}
+
+
+func GetFogPubkeyRust(recipient *PublicAddress) (*ristretto.Point, error) { // TODO need to also return pubkey expiry
+    report, err := GetFogReportResponse(recipient.FogReportUrl)
+    if err != nil {
+        return nil, err
+    }
+    reportBytes, err := proto.Marshal(report)
+    if err != nil {
+        return nil, err
+    }
+
+    fmt.Printf("report bytes %#v\n", len(reportBytes))
+
+    verifier, err := C.mc_verifier_create();
+    if err != nil {
+        return nil, err
+    }
+    fmt.Printf("verifier: %#v\n", verifier)
+
+    fog_resolver, err := C.mc_fog_resolver_create(verifier)
+    if err != nil {
+        return nil, err
+    }
+    fmt.Printf("fog_resolver: %#v\n", fog_resolver)
+
+    c_report_buf := C.CBytes(reportBytes)
+    defer C.free(c_report_buf)
+
+    report_buf := C.McBuffer { buffer: (*C.uchar)(c_report_buf), len: C.ulong(len(reportBytes)) }
+    fmt.Printf("report bytes %#v eheh\n", report_buf)
+
+    c_address := C.CString(recipient.FogReportUrl)
+	defer C.free(unsafe.Pointer(c_address))
+
+
+    ret, err := C.mc_fog_resolver_add_report_response(
+        fog_resolver,
+        c_address,
+        &report_buf,
+        nil,
+    )
+    if err != nil {
+        return nil, err
+    }
+    fmt.Printf("add report: %#v\n", ret)
+
+
+
+
+    // Go PublicAddress to libmobilecoin
+    view_bytes, err := hex.DecodeString(recipient.ViewPublicKey)
+    if err != nil { return nil, err }
+    c_view_bytes := C.CBytes(view_bytes)
+    defer C.free(c_view_bytes)
+
+    spend_bytes, err := hex.DecodeString(recipient.SpendPublicKey)
+    if err != nil { return nil, err }
+    c_spend_bytes := C.CBytes(spend_bytes)
+    defer C.free(c_spend_bytes)
+
+    authority_sig_bytes, err := hex.DecodeString(recipient.FogAuthoritySig)
+    if err != nil { return nil, err }
+    c_authority_sig_bytes := C.CBytes(authority_sig_bytes)
+    defer C.free(c_authority_sig_bytes)
+
+    c_report_url := C.CString(recipient.FogReportUrl)
+    defer C.free(unsafe.Pointer(c_report_url))
+
+    c_report_id := C.CString(recipient.FogReportId)
+    defer C.free(unsafe.Pointer(c_report_id))
+
+    c_authority_sig := (*C.McBuffer)(C.malloc(C.sizeof_McBuffer))
+    defer C.free(unsafe.Pointer(c_authority_sig))
+    c_authority_sig.buffer = (*C.uchar)(c_authority_sig_bytes)
+    c_authority_sig.len = C.ulong(len(authority_sig_bytes))
+
+    c_fog_info := (*C.McPublicAddressFogInfo)(C.malloc(C.sizeof_McPublicAddressFogInfo))
+    defer C.free(unsafe.Pointer(c_fog_info))
+    c_fog_info.report_url = c_report_url
+    c_fog_info.report_id = c_report_id
+    c_fog_info.authority_sig = c_authority_sig
+
+    c_view_public_key := (*C.McBuffer)(C.malloc(C.sizeof_McBuffer))
+    defer C.free(unsafe.Pointer(c_view_public_key))
+    c_view_public_key.buffer = (*C.uchar)(c_view_bytes)
+    c_view_public_key.len = C.ulong(len(view_bytes))
+
+    c_spend_public_key := (*C.McBuffer)(C.malloc(C.sizeof_McBuffer))
+    defer C.free(unsafe.Pointer(c_spend_public_key))
+    c_spend_public_key.buffer = (*C.uchar)(c_spend_bytes)
+    c_spend_public_key.len = C.ulong(len(spend_bytes))
+
+    c_public_address := (*C.McPublicAddress)(C.malloc(C.sizeof_McPublicAddress))
+    defer C.free(unsafe.Pointer(c_public_address))
+    c_public_address.view_public_key = c_view_public_key
+    c_public_address.spend_public_key = c_spend_public_key
+    c_public_address.fog_info = c_fog_info
+
+    fully_validated_fog_pub_key, err := C.mc_fog_resolver_get_fog_pubkey(
+        fog_resolver,
+        c_public_address,
+    )
+    if err != nil { return nil, err }
+
+    fmt.Printf("fully validated fog pub key: %#v\n", fully_validated_fog_pub_key)
+
+    return nil, nil
 }
