674f076db1ed445aa87abfbcf8a0e571367a2cb0
LiYuqing <im.yuqlee@gmail.com>
2022-07-26 23:04:32 +0800

    Add mc_transaction_builder_add_output

--- transaction_builder_c.go
+++ transaction_builder_c.go
@@ -9,12 +9,16 @@ package api
 // #include "libmobilecoin.h"
 import "C"
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
+
+	"github.com/bwesterb/go-ristretto"
+	account "github.com/jadeydi/mobilecoin-account"
 )
 
 // mc_transaction_builder_create
-func MCTransactionBuilderCreate(inputCs []*InputC, fee, tombstone uint64, version uint32) error {
+func MCTransactionBuilderCreate(inputCs []*InputC, amount, fee, tombstone uint64, version uint32, recipient *account.PublicAddress, change *account.Account) error {
 	verifier, err := C.mc_verifier_create()
 	if err != nil {
 		return err
@@ -98,4 +102,88 @@ func MCTransactionBuilderCreate(inputCs []*InputC, fee, tombstone uint64, versio
 			return fmt.Errorf("mc_transaction_builder_add_input failure")
 		}
 	}
+
+	// mc_transaction_builder_add_output
+	report_url_recipient_str := C.CString(recipient.FogReportUrl)
+	defer C.free(report_url_recipient_str)
+	report_id_recipient_str := C.CString(recipient.FogReportId)
+	defer C.free(report_id_recipient_str)
+	sigBuf, err := hex.DecodeString(recipient.FogAuthoritySig)
+	if err != nil {
+		return nil, err
+	}
+	sig_bytes := C.CBytes(sigBuf)
+	defer C.free(sig_bytes)
+	authority_sig := C.McBuffer{
+		buffer: (*C.uint8_t)(sig_bytes),
+		len:    C.size_t(len(sigBuf)),
+	}
+	fog_info := C.McPublicAddressFogInfo{
+		report_url:    (*C.char)(report_url_recipient_str),
+		report_id:     (*C.char)(report_id_recipient_str),
+		authority_sig: authority_sig,
+	}
+
+	viewPublicKeyBuf, err := hex.DecodeString(recipient.ViewPublicKey)
+	if err != nil {
+		return nil, err
+	}
+	view_public_key_bytes := C.CBytes(viewPublicKeyBuf)
+	defer C.free(view_public_key_bytes)
+	view_public := C.McBuffer{
+		buffer: (*C.uint8_t)(view_public_key_bytes),
+		len:    C.size_t(len(viewPublicKeyBuf)),
+	}
+
+	spendPublicKeyBuf, err := hex.DecodeString(recipient.SpendPublicKey)
+	if err != nil {
+		return nil, err
+	}
+	spend_public_key_bytes := C.CBytes(spendPublicKeyBuf)
+	defer C.free(spend_public_key_bytes)
+	spend_public := C.McBuffer{
+		buffer: (*C.uint8_t)(spend_public_key_bytes),
+		len:    C.size_t(len(spendPublicKeyBuf)),
+	}
+
+	recipient_address := &c.McPublicAddress{
+		view_public_key:  view_public,
+		spend_public_key: spend_public,
+		fog_info:         fog_info,
+	}
+
+	var rRecipient ristretto.Scalar
+	rRecipient.Rand()
+	viewPublicKeyRecipient := hexToPoint(recipient.ViewPublicKey)
+	secretRecipient := createSharedSecret(viewPublicKeyRecipient, &rRecipient)
+	secret_recipient_bytes := C.CBytes(secretRecipient.Bytes())
+	defer C.free(secret_recipient_bytes)
+	out_tx_out_shared_secret := &C.McMutableBuffer{
+		buffer: (*C.uint8_t)(secret_recipient_bytes),
+		len:    C.size_t(len(secretRecipient.Bytes())),
+	}
+
+	confirmationRecipientBuf := ConfirmationNumberFromSecret(secretRecipient)
+	confirmation_recipient_bytes := C.CBytes(confirmationRecipientBuf)
+	defer C.free(confirmation_recipient_bytes)
+	out_tx_out_confirmation_number := &C.McMutableBuffer{
+		buffer: (*C.uint8_t)(confirmation_recipient_bytes),
+		len:    C.size_t(len(confirmationRecipientBuf)),
+	}
+
+	var rng_callback *C.McRngCallback
+	var out_error *C.McError
+	_, err = C.mc_transaction_builder_add_output(transaction_builder, C.uint64_t(amount), recipient_address, rng_callback, out_tx_out_confirmation_number, out_tx_out_shared_secret, out_error)
+	if err != nil {
+		return nil, err
+	}
+	// mc_transaction_builder_add_change_output
+}
+
+func hexToBuf(text string) []byte {
+	buf, err := hex.DecodeString(text)
+	if err != nil {
+		panic(err)
+	}
+	return buf
 }
